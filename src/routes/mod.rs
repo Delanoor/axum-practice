e75243d2fdfa8be79e7544fe19850be92155ee5d
hwn0809 <hwn0809@gmail.com>
2024-03-03 13:18:06 +0900

    add headers & user-agent

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,13 +4,17 @@ use axum::{
 };
 mod hello;
 mod mirror;
+mod mirror_custom_header;
 mod mirror_json;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
 
 use hello::hello;
 use mirror::mirror;
+use mirror_custom_header::mirror_custom_header;
 use mirror_json::mirror_json;
+use mirror_user_agent::mirror_user_agent;
 use path_variables::path_variables;
 use query_params::query_params;
 
@@ -21,4 +25,6 @@ pub fn create_routes() -> Router {
         .route("/mirror-json", post(mirror_json))
         .route("/path/:id", get(path_variables))
         .route("/query_params", get(query_params))
+        .route("/mirror_user_agent", get(mirror_user_agent))
+        .route("/mirror_custom_header", get(mirror_custom_header))
 }
